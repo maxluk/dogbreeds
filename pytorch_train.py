9b4221e5073ed4e5796d5fa37dcdddf0596bcb01
Maxim Lukiyanov <maxiluk@gmail.com>
2018-12-09 17:53:27 -0800

    Adjust batch_size to 32 as a workaround for regression in AML GA version (https://msdata.visualstudio.com/Vienna/_workitems/edit/331811).

--- pytorch_train.py
+++ pytorch_train.py
@@ -45,8 +45,8 @@ def load_data(data_dir):
     image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
                                               data_transforms[x])
                       for x in ['train', 'val']}
-    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=128,
-                                        num_workers=4)
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=32,
+                                        num_workers=1)
                    for x in ['train', 'val']}
     dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
     class_names = image_datasets['train'].classes
